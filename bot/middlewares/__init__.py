389b4d64ad15aa38424946355eaeaeea64e177c7
k3l3vr4 <ayukhanov_nikita@gmail.com>
2023-08-29 17:02:17 +0600

    Reworked setup_middlewares

--- bot/middlewares/__init__.py
+++ bot/middlewares/__init__.py
@@ -10,9 +10,15 @@ from .trottling import ThrottlingMiddleware
 
 
 def setup_middlewares(dp: Dispatcher, pool: async_sessionmaker[AsyncSession], config_: Config):
-    for tmp in [dp.message, dp.callback_query, dp.my_chat_member, dp.error]:
+    for tmp in [dp.message, dp.callback_query, dp.my_chat_member]:
         tmp.middleware(LoggingMiddleware())
+
+    for tmp in [dp.message, dp.callback_query]:
+        tmp.middleware(ThrottlingMiddleware())
+
+    for tmp in [dp.message, dp.callback_query, dp.my_chat_member, dp.error]:
         tmp.middleware(ConfigMiddleware(config_))
+
+    for tmp in [dp.message, dp.callback_query, dp.my_chat_member]:
         tmp.middleware(DBMiddleware(pool))
         tmp.middleware(LoadDataMiddleware())
-        tmp.middleware(ThrottlingMiddleware())
