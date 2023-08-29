893ac6e41edd43a1448a799d9642f47ffbf0ffd3
k3l3vr4 <ayukhanov_nikita@gmail.com>
2023-08-29 17:00:17 +0600

    Fixed duplicate logging for sqlalchemy echo

--- bot/setup_logging.py
+++ bot/setup_logging.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 
 import yaml
+from sqlalchemy import log as sqlalchemy_log
 
 from bot.models.config.config import Config
 
@@ -20,11 +21,14 @@ class FileRotateNameHandler(logging.FileHandler):
 
 
 def setup_logging(config: Config):
+    if config.db.echo:
+        sqlalchemy_log._add_default_handler = lambda x: None
     try:
         with config.paths.logging.open("r") as f:
             logging_config = yaml.safe_load(f)
         if 'file_handler' not in logging_config['root']['handlers']:
             del logging_config['handlers']['file_handler']
+
         logging.config.dictConfig(logging_config)
         logger.info("Logging configured successfully")
     except IOError:
